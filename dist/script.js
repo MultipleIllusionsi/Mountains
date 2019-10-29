28661e413f64a7ec199b554e7e3b91b3d201b72e
MultipleIllusionsi <seregaserega06@gmail.com>
2019-10-30 01:17:17 +0500

    replaced ext script w/ my own script, added lazyloading for heavy background-img

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,30 @@
 (function() {
-  document.addEventListener("DOMContentLoaded", console.log("he"));
+  document.addEventListener("DOMContentLoaded", function() {
+    var lazyLoadImg = [].slice.call(
+      document.querySelectorAll(".lazyload")
+    );
+
+    if ("IntersectionObserver" in window) {
+      let sectionsObserver = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach(({ isIntersecting, target }) => {
+            if (isIntersecting) {
+              target.classList.remove("lazyload");
+              target.classList.add("lazyloaded");
+              sectionsObserver.unobserve(target);
+            }
+          });
+        }
+      );
+
+      lazyLoadImg.forEach(section =>
+        sectionsObserver.observe(section)
+      );
+    } else {
+      lazyLoadImg.forEach(section => {
+        section.classList.remove("lazyload");
+        section.classList.add("lazyloaded");
+      });
+    }
+  });
 })();
